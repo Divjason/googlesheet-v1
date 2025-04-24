8f4635754e5b1222c06e17056127853dfc7a8c0a
Divjason <sejinpark@tcocs.com>
2025-04-24 17:35:31 +0900

    fifth

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const form = document.getElementById("signup-form");
-const successMsg = document.getElementById("success-msg");
+// const form = document.getElementById("signup-form");
+// const successMsg = document.getElementById("success-msg");
 
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
+// form.addEventListener("submit", async function (e) {
+//   e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
 
-  console.log(email);
+//   console.log(email);
 
-  try {
-    const res = await fetch(
-      "https://script.google.com/macros/s/AKfycbwhzeT71WcDVlVWdHkLdlaOVWeKy_HgngnPcW0YOvxNnZcvlJ9LsOoRFPNmulPFDvVm/exec",
+//   try {
+//     const res = await fetch(
+//       "https://script.google.com/macros/s/AKfycbwhzeT71WcDVlVWdHkLdlaOVWeKy_HgngnPcW0YOvxNnZcvlJ9LsOoRFPNmulPFDvVm/exec",
 
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email }),
-      }
-    );
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ name, email }),
+//       }
+//     );
 
-    const result = await res.json();
-    if (result.result === "success") {
-      successMsg.textContent = "가입이 완료되었습니다!";
-      form.reset();
-    } else {
-      successMsg.textContent = "오류가 발생했습니다. 다시 시도해주세요.";
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    successMsg.textContent = "전송 실패 😢";
-  }
-});
+//     const result = await res.json();
+//     if (result.result === "success") {
+//       successMsg.textContent = "가입이 완료되었습니다!";
+//       form.reset();
+//     } else {
+//       successMsg.textContent = "오류가 발생했습니다. 다시 시도해주세요.";
+//     }
+//   } catch (error) {
+//     console.error("Error:", error);
+//     successMsg.textContent = "전송 실패 😢";
+//   }
+// });
